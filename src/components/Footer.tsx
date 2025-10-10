e8dfd4eec34bf6ecd75e7dbad4a3e4ca1f40311b
Mohamed Aref <maref.dev@gmail.com>
2025-10-10 14:25:34 +0000

    center  text at the center of the div (_for element div_)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ export function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-background/60 backdrop-blur-sm border-t border-border/40">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+      <div className="container flex flex-col items-center justify-center gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+          <p className="text-center text-sm leading-loose text-muted-foreground">
             &copy; {currentYear} Mohamed Aref. All rights reserved.
           </p>
         </div>
