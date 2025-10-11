46982cadfdbc00a83f6e3f6204b8703d7a00b630
Mohamed Aref <maref.dev@gmail.com>
2025-10-11 18:09:42 +0000

    add memoje with v reaction, use apple vision os  (_for element <Primitiv

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -24,7 +24,7 @@ export function About() {
         <div className="grid md:grid-cols-3 gap-12 items-start">
           <div className="flex flex-col items-center text-center md:col-span-1">
             <Avatar className="w-40 h-40 sm:w-48 sm:h-48 border-4 border-border/80 dark:border-white/20 shadow-2xl mb-4">
-              <AvatarImage src="https://firebasestudio.googleapis.com/v0/b/firebase-studio-users.appspot.com/o/images%2Fclwsdeyxi000b146q6g5zpjq2%2F1722513369136.jpeg?alt=media&token=240b9919-0118-4e67-b529-37f2a16c7069" alt="Mohamed Aref" />
+              <AvatarImage src="https://firebasestudio.googleapis.com/v0/b/firebase-studio-users.appspot.com/o/images%2Fclwsdeyxi000b146q6g5zpjq2%2F1722896561139.jpeg?alt=media&token=489dc84c-f09d-43ab-898f-0a0224097950" alt="Mohamed Aref" />
               <AvatarFallback>MA</AvatarFallback>
             </Avatar>
             <h2 className="text-3xl sm:text-4xl font-bold tracking-tighter font-headline mt-4">Mohamed Aref</h2>
