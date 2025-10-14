ac7a66e30297d02249ea5983f8167f6635688ec6
Mohamed Aref <maref.dev@gmail.com>
2025-10-14 14:20:38 +0000

    Try fixing this error: `Runtime Error: TypeError: Super expression must

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { DollarSign, Users, CreditCard, Activity, ArrowUp } from 'lucide-react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid } from 'recharts';
