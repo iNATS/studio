72d9a630ff78be104babe20944447183d4d9e068
Mohamed Aref <maref.dev@gmail.com>
2025-11-26 14:48:22 +0000

    now i need to push to this https://github.com/iNATS/mohamed-aref-portfol

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/mohamed-aref-portfolio',
+  assetPrefix: '/mohamed-aref-portfolio/',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +11,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
